a5237ed4b411c96f0b7e978667fd277ae129b086
Abbas Sadeghi <abbas.sadeghi1384@gmail.com>
2026-01-07 02:33:24 +0330

    add tailwind config file

---
+++ tailwind-config.js
@@ -0,0 +1,14 @@
+window.tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#38BDF8',
+        secondary: '#EF4444',
+      },
+      fontFamily: {
+        bbh: ['"BBH Bogle"', 'sans-serif'],
+        display: ['Oswald'],
+      },
+    },
+  },
+};
